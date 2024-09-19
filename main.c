f40906f59b3c30db5a97bf87fd5700ee4e036d71
HenriUz <d2023001316@unifei.edu.br>
2024-09-19 19:37:49 -0300

    Funções testadas e funcionando corretamente.

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ Visualização de uma matriz 3x3:
 | [j, k, l] [m, n, o] [p, q, r] |
 | [s, t, u] [v, w, x] [y, z, ç] |
 Note que cada elemento da matriz é um vetor de 3 elementos, simulando um píxel RGB.
+
+Importante: na maioria dos loops, está sendo feito somente uma verificação, mesmo com muitas variáveis sendo declaradas. Isso acontece pelo fato do intervalo (i2 - i1 ou j2 - j1) sempre ser o mesmo, independente do quadrante.
 */
 
 
@@ -32,13 +34,11 @@ Entrada: matriz, dimensão n.
 Saída: nada.
 */
 void imprimeMatriz(int ***matriz, int n) {
-    printf("\nImprimindo matriz:\n");
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            for(int k = 0; k < 3; k++) {
+    for(int i = 0; i < n; i++) { //Percorrendo as linhas.
+        for(int j = 0; j < n; j++) { //Percorrendo as colunas.
+            for(int k = 0; k < 3; k++) { //Percorrendo os píxels.
                 printf("%d ", matriz[i][j][k]);
             }
-            printf("| ");
         }
         printf("\n");
     }
@@ -53,7 +53,7 @@ void multiplicarMatrizes(int ***matrizO, int ***matrizF, int ***matrizR, interva
     int elemento; //Vai armazenar o valor de cada elemento da matriz resultante.
 
     /* Fazendo a multiplicação. */
-    for(int iO = inO->i1, iR = 0; iO < inO->i2; iO++, iR++) { //Percorrendo as linhas (iO - linha da matrizO, iR - linha da matrizR). Somente uma verificação, pois a área das matrizes sempre será a mesma.
+    for(int iO = inO->i1, iR = 0; iO < inO->i2; iO++, iR++) { //Percorrendo as linhas (iO - linha da matrizO, iR - linha da matrizR).
         for(int jF = inF->j1, jR = 0; jF < inF->j2; jF++, jR++) { //Percorrendo as colunas (jF - coluna da matrizF, jR - coluna da matrizR).
             for(int k = 0; k < 3; k++) { //Percorrendo o vetor dos píxels.
                 elemento = 0;
@@ -72,24 +72,38 @@ Entrada: matriz original, matriz filtro, matriz resultante, intervalo da matriz 
 Saída: nada.
 */
 void strassen(int ***matrizO, int ***matrizF, int ***matrizR, intervalo *inO, intervalo *inF, int n) {
-    intervalo inAE, inBF, inCG, inDH; //Intervalos dos quadrantes A e E, B e F, C e G, D e H.
+    intervalo inqA, inqB, inqC, inqD, inqE, inqF, inqG, inqH; //Intervalos dos quadrantes.
     intervalo in0new; //Intervalo das matrizes de operações básicas e de produtos (0 até newN).
     int ***P1, ***P2, ***P3, ***P4, ***P5, ***P6, ***P7; //Os sete produtos.
-    int ***bas1, ***bas2, ***bas3, ***bas4, ***bas5, ***bas6, ***bas7, ***bas8, ***bas9, ***bas10; //Matrizes resultantes das 10 operações básicas.
+    int ***bas1, ***bas2, ***bas3, ***bas4, ***bas5, ***bas6, ***bas7, ***bas8, ***bas9, ***bas10; //Matrizes resultantes das 10 operações básicas. As 4 primeiras também serão utilizadas para o calcular os quadrantes finais.
     int newN = n/2; //Nova dimensão (newN x newN).
 
-    if(n <= 2) {
+    if(n <= 32) {
         multiplicarMatrizes(matrizO, matrizF, matrizR, inO, inF); //Multiplicando as matrizes com o algoritmo primitivo.
     }else {
         /* Setando as linhas dos quadrantes. */
-        inAE.i1 = inBF.i1 = in0new.i1 = 0;
-        inAE.i2 = inBF.i2 = inCG.i1 = inDH.i1 = in0new.i2 = newN;
-        inCG.i2 = inDH.i2 = n;
+        in0new.i1 = 0;
+        in0new.i2 = newN;
+
+        inqA.i1 = inqB.i1 = inO->i1;
+        inqA.i2 = inqB.i2 = inqC.i1 = inqD.i1 = (inO->i1 + inO->i2)/2;
+        inqC.i2 = inqD.i2 = inO->i2;
+
+        inqE.i1 = inqF.i1 = inF->i1;
+        inqE.i2 = inqF.i2 = inqG.i1 = inqH.i1 = (inF->i1 + inF->i2)/2;
+        inqG.i2 = inqH.i2 = inF->i2;
 
         /* Setando as colunas dos quadrantes. */
-        inAE.j1 = inCG.j1 = in0new.j1 = 0;
-        inAE.j2 = inCG.j2 = inBF.j1 = inDH.j1 = in0new.j2 = newN;
-        inBF.j2 = inDH.j2 = n;
+        in0new.j1 = 0;
+        in0new.j2 = newN;
+
+        inqA.j1 = inqC.j1 = inO->j1;
+        inqA.j2 = inqC.j2 = inqB.j1 = inqD.j1 = (inO->j1 + inO->j2)/2;
+        inqB.j2 = inqD.j2 = inO->j2;
+
+        inqE.j1 = inqG.j1 = inF->j1;
+        inqE.j2 = inqG.j2 = inqF.j1 = inqH.j1 = (inF->j1 + inF->j2)/2;
+        inqF.j2 = inqH.j2 = inF->j2;
 
         /* Alocando espaço para os produtos e operações básicas. */
         {
@@ -151,28 +165,28 @@ void strassen(int ***matrizO, int ***matrizF, int ***matrizR, intervalo *inO, in
         }
 
         /* Calculando as matrizes resultantes das operações básicas (+ ou -), para o cálculo dos produtos. */
-        for(int linha1 = inAE.i1, linha2 = inCG.i1, i = 0; linha1 < inAE.i2; linha1++, linha2++, i++) { //Percorrendo as linhas dos quadrantes.
-            for(int coluna1 = inAE.j1, coluna2 = inBF.j1, j = 0; coluna1 < inAE.j2; coluna1++, coluna2++, j++) { //Percorrendo as colunas dos quadrantes.
+        for(int linhaA = inqA.i1, linhaC = inqC.i1, linhaE = inqE.i1, linhaG = inqG.i1, i = 0; linhaA < inqA.i2; linhaA++, linhaC++, linhaE++, linhaG++, i++) { //Percorrendo as linhas dos quadrantes.
+            for(int colunaA = inqA.j1, colunaB = inqB.j1, colunaE = inqE.j1, colunaF = inqF.j1, j = 0; colunaA < inqA.j2; colunaA++, colunaB++, colunaE++, colunaF++, j++) { //Percorrendo as colunas dos quadrantes.
                 for(int k = 0; k < 3; k++) { //Percorrendo os píxels.
-                    bas1[i][j][k] = matrizF[linha1][coluna2][k] - matrizF[linha2][coluna2][k]; //F - H
-                    bas2[i][j][k] = matrizO[linha1][coluna1][k] + matrizO[linha1][coluna2][k]; //A + B
-                    bas3[i][j][k] = matrizO[linha2][coluna1][k] + matrizO[linha2][coluna2][k]; //C + D
-                    bas4[i][j][k] = matrizF[linha2][coluna1][k] - matrizF[linha1][coluna1][k]; //G - E
-                    bas5[i][j][k] = matrizO[linha1][coluna1][k] + matrizO[linha2][coluna2][k]; //A + D
-                    bas6[i][j][k] = matrizF[linha1][coluna1][k] + matrizF[linha2][coluna2][k]; //E + H
-                    bas7[i][j][k] = matrizO[linha1][coluna2][k] - matrizO[linha2][coluna2][k]; //B - D
-                    bas8[i][j][k] = matrizF[linha2][coluna1][k] + matrizF[linha2][coluna2][k]; //G + H
-                    bas9[i][j][k] = matrizO[linha1][coluna1][k] - matrizO[linha2][coluna1][k]; //A - C
-                    bas10[i][j][k] = matrizF[linha1][coluna1][k] + matrizF[linha1][coluna2][k]; //E + F
+                    bas1[i][j][k] = matrizF[linhaE][colunaF][k] - matrizF[linhaG][colunaF][k]; //F - H
+                    bas2[i][j][k] = matrizO[linhaA][colunaA][k] + matrizO[linhaA][colunaB][k]; //A + B
+                    bas3[i][j][k] = matrizO[linhaC][colunaA][k] + matrizO[linhaC][colunaB][k]; //C + D
+                    bas4[i][j][k] = matrizF[linhaG][colunaE][k] - matrizF[linhaE][colunaE][k]; //G - E
+                    bas5[i][j][k] = matrizO[linhaA][colunaA][k] + matrizO[linhaC][colunaB][k]; //A + D
+                    bas6[i][j][k] = matrizF[linhaE][colunaE][k] + matrizF[linhaG][colunaF][k]; //E + H
+                    bas7[i][j][k] = matrizO[linhaA][colunaB][k] - matrizO[linhaC][colunaB][k]; //B - D
+                    bas8[i][j][k] = matrizF[linhaG][colunaE][k] + matrizF[linhaG][colunaF][k]; //G + H
+                    bas9[i][j][k] = matrizO[linhaA][colunaA][k] - matrizO[linhaC][colunaA][k]; //A - C
+                    bas10[i][j][k] = matrizF[linhaE][colunaE][k] + matrizF[linhaE][colunaF][k]; //E + F
                 }
             }
         }
 
         /* Calculando os 7 produtos. */
-        strassen(matrizO, bas1, P1, &inAE, &in0new, newN); //P1 = A(F - H)
-        strassen(bas2, matrizF, P2, &in0new, &inDH, newN); //P2 = (A + B)H
-        strassen(bas3, matrizF, P3, &in0new, &inAE, newN); //P3 = (C + D)E
-        strassen(matrizO, bas4, P4, &inDH, &in0new, newN); //P4 = D(G - E)
+        strassen(matrizO, bas1, P1, &inqA, &in0new, newN); //P1 = A(F - H)
+        strassen(bas2, matrizF, P2, &in0new, &inqH, newN); //P2 = (A + B)H
+        strassen(bas3, matrizF, P3, &in0new, &inqE, newN); //P3 = (C + D)E
+        strassen(matrizO, bas4, P4, &inqD, &in0new, newN); //P4 = D(G - E)
         strassen(bas5, bas6, P5, &in0new, &in0new, newN); //P5 = (A + D)(E + H)
         strassen(bas7, bas8, P6, &in0new, &in0new, newN); //P6 = (B - D)(G + H)
         strassen(bas9, bas10, P7, &in0new, &in0new, newN); //P7 = (A - C)(E + F)
@@ -272,10 +286,12 @@ void strassen(int ***matrizO, int ***matrizF, int ***matrizR, intervalo *inO, in
 }
 
 int main(void) {
-    FILE *imagem = fopen("C:\\Users\\Henrique Zucato\\Documents\\01-Universidade\\4-Periodo\\CTCO04-Projeto-E-Analise-De-Algoritmos\\Trabalho-01-Multiplicacao-Matrizes\\Codigos\\ppm\\Exemplo1.txt", "r");
+    FILE *imagem = fopen("C:\\Users\\Henrique Zucato\\Documents\\01-Universidade\\4-Periodo\\CTCO04-Projeto-E-Analise-De-Algoritmos\\Trabalho-01-Multiplicacao-Matrizes\\Codigos\\ppm\\10.in", "r");
+    //FILE *outEsp = fopen("C:\\Users\\Henrique Zucato\\Documents\\01-Universidade\\4-Periodo\\CTCO04-Projeto-E-Analise-De-Algoritmos\\Trabalho-01-Multiplicacao-Matrizes\\Codigos\\ppm\\10.out", "r");
     char codificacao[3]; //Codificação da imagem.
     int c; //Maior pixel.
     int ***matrizO, ***matrizF, ***matrizR; //Matriz original (imagem), matriz filtro, matriz resultado (imagem após o filtro).
+    //int ***matEsp;
     intervalo inImagem; //Intervalo da matriz da imagem.
 
     /* Verificando se a imagem abriu. */
@@ -293,6 +309,7 @@ int main(void) {
 
     /* Lendo a dimensão e maior píxel. */
     inImagem.i1 = inImagem.j1 = 0;
+
     fscanf(imagem, " %d", &inImagem.i2);
     fscanf(imagem, " %d", &inImagem.j2);
     fscanf(imagem, " %d", &c);
@@ -301,14 +318,17 @@ int main(void) {
     matrizO = (int ***)malloc(sizeof(int **) * inImagem.i2);
     matrizF = (int ***)malloc(sizeof(int **) * inImagem.i2);
     matrizR = (int ***)malloc(sizeof(int **) * inImagem.i2);
+    //matEsp = (int ***)malloc(sizeof(int **) * inImagem.i2);
     for(int i = 0; i < inImagem.i2; i++) {
         matrizO[i] = (int **)malloc(sizeof(int *) * inImagem.j2);
         matrizF[i] = (int **)malloc(sizeof(int *) * inImagem.j2);
         matrizR[i] = (int **)malloc(sizeof(int *) * inImagem.j2);
+        //matEsp[i] = (int **)malloc(sizeof(int *) * inImagem.j2);
         for(int j = 0; j < inImagem.j2; j++) {
             matrizO[i][j] = (int *)malloc(sizeof(int) * 3);
             matrizF[i][j] = (int *)malloc(sizeof(int) * 3);
             matrizR[i][j] = (int *)malloc(sizeof(int) * 3);
+            //matEsp[i][j] = (int *)malloc(sizeof(int) * 3);
         }
     }
 
@@ -317,6 +337,7 @@ int main(void) {
         for(int j = 0; j < inImagem.j2; j++) {
             for(int k = 0; k < 3; k++) {
                 fscanf(imagem, "%d", &matrizO[i][j][k]);
+                //fscanf(outEsp, "%d", &matEsp[i][j][k]);
             }
         }
     }
@@ -328,12 +349,23 @@ int main(void) {
         }
     }
 
-    imprimeMatriz(matrizO, inImagem.i2);
-    imprimeMatriz(matrizF, inImagem.i2);
-
-    /* Testando. */
+    /* Aplicando o filtro (multiplicando por meio do algoritmo de Strassen). */
     strassen(matrizO, matrizF, matrizR, &inImagem, &inImagem, inImagem.i2);
-    imprimeMatriz(matrizR, inImagem.i2);
+    printf("%s", codificacao);
+    printf("\n%d %d", inImagem.i2, inImagem.i2);
+    printf("\n%d\n", c);
+    //imprimeMatriz(matrizR, inImagem.i2);
+
+    /* Verificando igualdade. */
+    // for(int i = 0; i < inImagem.i2; i++) {
+    //     for(int j = 0; j < inImagem.j2; j++) {
+    //         for(int k = 0; k < 3; k++) {
+    //             if(matrizR[i][j][k] != matEsp[i][j][k]) {
+    //                 printf("\nErro identificado - %d %d %d - %d %d.", i, j, k, matrizR[i][j][k], matEsp[i][j][k]);
+    //             }
+    //         }
+    //     }
+    // }
 
     /* Liberando memória e fechando o arquivo. */
     for(int i = 0; i < inImagem.i2; i++) {
@@ -341,14 +373,17 @@ int main(void) {
             free(matrizO[i][j]);
             free(matrizF[i][j]);
             free(matrizR[i][j]);
+            //free(matEsp[i][j]);
         }
         free(matrizO[i]);
         free(matrizF[i]);
         free(matrizR[i]);
+        //free(matEsp[i]);
     }
     free(matrizO);
     free(matrizF);
     free(matrizR);
+    //free(matEsp);
     fclose(imagem);
     return 0;
 }
